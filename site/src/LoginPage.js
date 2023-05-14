5eb8f0a39e022b5ee76e92e66dda347ddb0a12e3
cleber2010 <cleber2020casagrande@gmail.com>
2023-05-14 00:19:38 -0300

    login

---
+++ site/src/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignInSide from './SignInSide';
+
+function LoginPage() {
+  return (
+    <div>
+      <SignInSide />
+    </div>
+  );
+}
+
+export default LoginPage;

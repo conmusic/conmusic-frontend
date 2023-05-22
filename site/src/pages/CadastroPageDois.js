2e6d0e51d13d893727d3869b2b219cc8d4ea30ac
Cleber Casagrande <cleber.casagrande@matrixenergia.com>
2023-05-22 18:51:11 -0300

    pagina de cadastro

---
+++ site/src/pages/CadastroPageDois.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cadastro from './cadastroDois';
+
+function CadastroPageDois() {
+  return (
+    <div>
+      <Cadastro />
+    </div>
+  );
+}
+
+export default CadastroPageDois;

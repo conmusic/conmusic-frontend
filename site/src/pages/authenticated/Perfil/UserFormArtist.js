08f21753c80c9f52ce8063cb3a48455f6eb36c11
cleber2010 <cleber2020casagrande@gmail.com>
2023-11-13 00:16:45 -0300

    pequenas alterações

--- site/src/pages/authenticated/Perfil/UserFormArtist.js
+++ site/src/pages/authenticated/Perfil/UserFormArtist.js
@@ -53,10 +53,43 @@ export default function UserFormArtist(onUpload) {
         setSelectedFile(file);
     };
 
-    const handleUpload = () => {
-        if (selectedFile) {
-            onUpload(selectedFile);
-            setSelectedFile(null);
+    const handleUpload = async () => {
+
+        if (!selectedFile) {
+            try {
+                const formData = new FormData();
+                // formData.append('image', selectedFile);
+                formData.append('name', cardData.length > 0 ? cardData[0].name : '');
+                formData.append('email', cardData.length > 0 ? cardData[0].email : '');
+                formData.append('phone', cardData.length > 0 ? cardData[0].phoneNumber : '');
+                // Adicione outros campos do formulário aqui
+
+                // Obtenha o token de autenticação
+                const token = localStorage.getItem('@conmusic:token');
+
+                // Configure os cabeçalhos com o token
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                };
+
+                // Chame a função de upload (substitua pela sua lógica real)
+                const response = await api.post(`/artists/${userId}`, formData, config);
+
+                // Lide com a resposta, se necessário
+                console.log('Resposta do upload:', response);
+
+                // Limpe o estado após o envio
+                setSelectedFile(null);
+
+                // Exiba uma mensagem de sucesso ou faça outras ações necessárias
+                alert('Upload bem-sucedido!');
+            } catch (error) {
+                // Lide com erros durante o upload
+                console.error('Erro durante o upload:', error);
+                alert('Erro durante o upload. Consulte o console para obter detalhes.');
+            }
         }
     };
     const [selectedImage] = useState(0);
@@ -64,41 +97,61 @@ export default function UserFormArtist(onUpload) {
         'https://s2-g1.glbimg.com/u_Sep5KE8nfnGb8wWtWB-vbBeD0=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/N/Q/S27GlHSKA6DAAjshAgSA/bar-paradiso.png',
     ]
 
+    // Define um efeito colateral que é executado quando o valor de `userId` muda
     useEffect(() => {
+        // Função assíncrona para obter dados de artistas do servidor
         const getArtistsData = async () => {
             try {
                 var token = localStorage.getItem('@conmusic:token');
                 const config = {
                     headers: { Authorization: `Bearer ${token}` }
                 };
-                console.log("userId:", userId)
-                const response = await api.get(`/artists/${userId}`, config)
-                console.log("data:", response)
-                var card = response.data
-                    .map(obj => {
-                        return {
-                            id: obj.id,
-                            name: obj.name,
-                            email: obj.email,
-                            cpf: obj.cpf,
-                            phoneNumber: obj.phoneNumber,
-                            birthDate: obj.birthDate,
-                            about: obj.about,
-                            instagram: obj.instagram,
-                        }
-                    })
 
+                // Registra o valor atual de `userId` no console
+                console.log("userId:", userId);
+
+
+
+                // Faz uma requisição GET ao servidor para obter dados do artista
+                const response = await api.get(`/artists/${userId}`, config);
+
+                // Registra os dados recebidos do servidor no console
+                console.log("data:", response);
+
+                // Verifica se a resposta do servidor é uma matriz
+                const card = Array.isArray(response.data)
+                    // Se for uma matriz, mapeia os dados do artista
+                    ? response.data.map(obj => ({
+                        id: obj.id,
+                        name: obj.name,
+                        email: obj.email,
+                        cpf: obj.cpf,
+                        phoneNumber: obj.phoneNumber,
+                        birthDate: obj.birthDate,
+                        about: obj.about,
+                        instagram: obj.instagram,
+                    }))
+                    // Se não for uma matriz, assume que é um objeto e coloca-o em uma matriz
+                    : [response.data];
+                console.log("mapped card data:", card);
+
+
+                // Configura o estado `cardData` com os dados obtidos do servidor
                 setCardData(card);
             } catch (error) {
+                // Trata erros durante a solicitação ao servidor
                 console.error('Erro ao buscar os dados dos cards:', error);
-                console.error('Mensagem de erro do servidor:', error.response.data);
+                console.error('Mensagem de erro do servidor:', error.response ? error.response.data : error.message);
             }
         };
+
+        // Verifica se `userId` não é zero antes de chamar a função para buscar dados
         if (userId !== 0) {
             getArtistsData();
         }
-    }, [userId]);
-    
+    }, [userId]); // Dependência do efeito, ou seja, o efeito será reexecutado se `userId` mudar
+
+
 
     return (
         <React.Fragment >
@@ -111,7 +164,7 @@ export default function UserFormArtist(onUpload) {
                     Dados do Usuário
                 </Typography>
                 <Grid container spacing={3}>
-                    
+
                     <Grid item xs={12} sm={6}>
                         <TextField
                             required
@@ -133,6 +186,7 @@ export default function UserFormArtist(onUpload) {
                             fullWidth
                             autoComplete="family-name"
                             variant="outlined"
+                            defaultValue={cardData.length > 0 ? cardData[0].email : ''}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -147,6 +201,7 @@ export default function UserFormArtist(onUpload) {
                                 maxLength: 15,
                                 pattern: "{2} {5}-{4}",
                             }}
+                            defaultValue={cardData.length > 0 ? cardData[0].phoneNumber : ''}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -161,6 +216,7 @@ export default function UserFormArtist(onUpload) {
                                 maxLength: 14,
                                 pattern: "{3}{3}{3}-{2}",
                             }}
+                            defaultValue={cardData.length > 0 ? cardData[0].cpf : ''}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -176,6 +232,7 @@ export default function UserFormArtist(onUpload) {
                                 maxLength: 10,
                                 pattern: "{2}/{2}/{4}",
                             }}
+                            defaultValue={cardData.length > 0 ? cardData[0].birthDate : ''}
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -185,6 +242,7 @@ export default function UserFormArtist(onUpload) {
                             label="Instagram"
                             fullWidth
                             variant="outlined"
+                            defaultValue={cardData.length > 0 ? cardData[0].instagram : ''}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -196,6 +254,7 @@ export default function UserFormArtist(onUpload) {
                             fullWidth
                             autoComplete="shipping address-line1"
                             variant="outlined"
+
                         />
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -261,9 +320,11 @@ export default function UserFormArtist(onUpload) {
                                 color: 'white',
                                 marginTop: 2
                             }}
+                            onClick={handleUpload}
                         >
                             Salvar
                         </Button>
+
                         <Button
                             variant="contained"
                             size="medium"

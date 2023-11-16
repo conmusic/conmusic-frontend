4719212b1201c740a1d766ded907a336c0403297
GuilhermeAlvesBarros <guilherme.abarros@sptech.school>
2023-11-15 23:21:38 -0300

    steps para criacao de estabelecimento

--- src/pages/authenticated/ManageEstablishment.js
+++ src/pages/authenticated/ManageEstablishment.js
@@ -18,6 +18,11 @@ import api from '../../services/api';
 import Title from '../../components/Title';
 import { useAuth } from '../../hooks/auth';
 import eventPropsHelper from '../../helpers/eventPropsHelper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = ['Informações gerais', 'Infraestrutura', 'Localização'];
 
 const style = {
     position: 'absolute',
@@ -35,12 +40,13 @@ const style = {
     justifyContent: 'space-between',
     height: 'auto',
     alignItems: 'center',
-    gap: 2
+    gap: 2,
+    overflowY: "scroll",
+    maxHeight: "100%"
 };
 
 export default function ManageEstablishment() {
     const { userId } = useAuth();
-
     const [establishments, setEstablishments] = useState([])
 
     const [openCreateModal, setOpenCreateModal] = useState(false);
@@ -71,8 +77,8 @@ export default function ManageEstablishment() {
                         city: establishment.city,
                         state: establishment.state
                     },
-                    fantasyName: establishment.fantasyName,                    
-                    establishmentName: establishment.establishmentName,                    
+                    fantasyName: establishment.fantasyName,
+                    establishmentName: establishment.establishmentName,
                     phoneNumber: establishment.phoneNumber,
                     cnpj: establishment.cnpj
                 })))
@@ -93,26 +99,72 @@ export default function ManageEstablishment() {
         }))
     }
 
+    const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
     return (
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Stack spacing={2} direction="row" justifyContent={'space-between'}>
                 <Title>Estabelecimentos</Title>
-                <Button variant="contained" color="success">
+                <Button variant="contained" color="success" onClick={() => setOpenCreateModal(true)}>
                     Criar Estabelecimento
                 </Button>
+
             </Stack>
             <Grid container spacing={0.5} sx={{ mt: 3 }}>
                 {
                     establishments.map(establishment => (
-                        <Grid 
-                            key={`Grid#${establishment.id}`} 
-                            item 
-                            xs={12} 
-                            md={12} 
-                            lg={12} 
+                        <Grid
+                            key={`Grid#${establishment.id}`}
+                            item
+                            xs={12}
+                            md={12}
+                            lg={12}
                             sx={{ display: "flex" }}
                         >
-                            <Card key={`Card#${establishment.id}`} sx={{display: 'flex', width: 1300, gap: 10 }}>
+                            <Card key={`Card#${establishment.id}`} sx={{ display: 'flex', width: 1300, gap: 10 }}>
                                 <CardMedia
                                     key={`CardImage#${establishment.id}`}
                                     component="img"
@@ -126,7 +178,7 @@ export default function ManageEstablishment() {
                                     }}
                                     src="https://source.unsplash.com/random?wallpapers"
                                 />
-                                <CardContent key={`EstablishmentData#${establishment.id}`} sx={{  mt: 1, flex: 1 }}>
+                                <CardContent key={`EstablishmentData#${establishment.id}`} sx={{ mt: 1, flex: 1 }}>
                                     <Typography key={`EstablishmentName#${establishment.id}`} component="h2" variant="h5">
                                         {establishment.fantasyName}
                                     </Typography>
@@ -144,15 +196,15 @@ export default function ManageEstablishment() {
                                     </Typography>
                                 </CardContent>
                                 <CardContent key={`Manage#${establishment.id}`} xs={12} md={4} lg={4} style={{ display: 'flex', alignItems: 'center' }}>
-                                    <Button 
+                                    <Button
                                         key={`ManageButton#${establishment.id}`}
                                         variant="contained"
                                         style={{
-                                            display: 'flex', 
+                                            display: 'flex',
                                             backgroundColor: '#FB2D57',
-                                            color: 'white', 
-                                            width: 120, 
-                                            height: 40, 
+                                            color: 'white',
+                                            width: 120,
+                                            height: 40,
                                             marginRight: 15
                                         }}
                                     >
@@ -162,7 +214,7 @@ export default function ManageEstablishment() {
                             </Card>
                         </Grid>
                     ))
-                }                
+                }
             </Grid>
             <Modal
                 open={openCreateModal}
@@ -174,9 +226,27 @@ export default function ManageEstablishment() {
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         Criar Estabelecimento
                     </Typography>
+
+                    <Box sx={{ width: '100%' }}>
+                        <Stepper activeStep={activeStep}>
+                            {steps.map((label, index) => {
+                                const stepProps = {};
+                                const labelProps = {};
+                                if (isStepSkipped(index)) {
+                                    stepProps.completed = false;
+                                }
+                                return (
+                                    <Step key={label} {...stepProps}>
+                                        <StepLabel {...labelProps}>{label}</StepLabel>
+                                    </Step>
+                                );
+                            })}
+                        </Stepper>
+                        
+                    </Box>
                     <Box
                         component="form"
-                        sx={{ display: 'flex', flexDirection: 'column', gap: 2  }}
+                        sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
                         noValidate
                         autoComplete="off"
                     >
@@ -282,6 +352,35 @@ export default function ManageEstablishment() {
                             />
                         </div>
                     </Box>
+                    {activeStep === steps.length ? (
+                            <React.Fragment>
+                                <Typography sx={{ mt: 2, mb: 1 }}>
+                                    Todos os passos foram concluidos!
+                                </Typography>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                    <Box sx={{ flex: '1 1 auto' }} />
+                                </Box>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                <Typography sx={{ mt: 1, mb: 1 }}>Passo {activeStep + 1}</Typography>
+                                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                    <Button
+                                        color="inherit"
+                                        disabled={activeStep === 0}
+                                        onClick={handleBack}
+                                        sx={{ mr: 1 }}
+                                    >
+                                        Voltar
+                                    </Button>
+                                    <Box sx={{ flex: '1 1 auto' }} />
+
+                                    <Button onClick={handleNext}>
+                                        {activeStep === steps.length - 1 ? 'Finalizar' : 'Próximo'}
+                                    </Button>
+                                </Box>
+                            </React.Fragment>
+                        )}
                     <Button variant="contained" color="success" style={{ width: 250, height: 40 }}>
                         Criar Estabelecimento
                     </Button>
